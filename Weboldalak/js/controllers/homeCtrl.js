08b4296f5a3b3741b55dc3b307ef0bb7b6fad0e3
Logfer <molnar.viktor999@gmail.com>
2022-11-08 09:40:35 +0100

    kezdő szenvedés

--- Weboldalak/js/controllers/homeCtrl.js
+++ Weboldalak/js/controllers/homeCtrl.js
@@ -1,3 +1,14 @@
 app.controller('homeCtrl', function($scope) {
-
+    $scope.adatok = 
+    [
+        {
+            oldalcim: "Síkideg"
+        },
+        {
+            cim: "Bemutatkozás"
+        },
+        {
+            szoveg: "A síkidegek csapata annak céljából álltak össze, hogy egy kényelmesen használható programot hozzanak létre terület-kerület(Síkbeli), illetve térfogat-felszín(térbeli) számításokra."
+        }
+    ]
 });
