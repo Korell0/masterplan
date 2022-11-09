ff2c4e0766d3638fc7141e03144cc82e0b2ecd77
beretkag <beretkag@gmail.com>
2022-11-09 11:02:20 +0100

    sik 4 helyreállítása

---
+++ MasterPlanProgram/MasterPlanProgram/Gomb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MasterPlanProgram
+{
+    class Gomb
+    {
+        List<double> Adatok;
+        List<Label> Cimkek;
+        List<TextBox> Bemenet;
+
+
+        public Gomb()
+        {
+
+        }
+    }
+}
