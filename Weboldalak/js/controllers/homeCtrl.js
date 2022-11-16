fdf8cd64f44c37c34ac57455a6837c3423423c9c
Logfer <molnar.viktor999@gmail.com>
2022-11-16 10:05:48 +0100

    Weboldalak hotfixe

--- Weboldalak/js/controllers/homeCtrl.js
+++ Weboldalak/js/controllers/homeCtrl.js
@@ -3,9 +3,9 @@ app.controller('homeCtrl', function($scope) {
     $scope.cim= "Bemutatkozás"
     $scope.szoveg= "A síkidegek csapata annak céljából álltak össze, hogy egy kényelmesen használható programot hozzanak létre terület-kerület(Síkbeli), illetve térfogat-felszín(térbeli) számításokra."
 
-    $scope.gellert= "Gellért"
-    $scope.adam= "Ádám"
-    $scope.viktor= "Viktor"
+    $scope.gellert= "Síkidegek programjáért felelt"
+    $scope.adam= "Program oldal szerkezetéért és annak formázásáért felelt"
+    $scope.viktor= "Fő oldal szerkezetéért és annak formázásáért felelt"
 
     $scope.progi="/program"
 });
